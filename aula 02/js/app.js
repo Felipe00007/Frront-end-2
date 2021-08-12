9ffb520a36aa714c0329ffa65337deffa09b461a
FELIPE RODRIGUES DE MELO <85718153+Felipe00007@users.noreply.github.com>
2021-08-12 02:16:54 -0300

    Front

---
+++ aula 02/js/app.js
@@ -0,0 +1,11 @@
+let situacao = confirm("Para acessar o Sistema - pressione ok");
+
+if(situacao) {
+    
+    window.location.href="acessoPremitido.html";
+
+} else{
+    window.location.href="acessoNegado.html";
+    
+}
+
